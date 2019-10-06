6c0971d787f4ed760625b325880c7ac15a4b5ab2
Jake Zaia <jlz2369@g.rit.edu>
2019-10-05 20:36:02 -0400

    Implement basic mongoutils

--- theinternetofthings/utils/cloudFunctions.py
+++ theinternetofthings/utils/cloudFunctions.py
@@ -10,7 +10,7 @@ def getImageContents(img_url):
     return eval(r.text)
 
 def getImageText(img_url):
-    url = "https://us-central1-theinternetofthings.cloudfunctions.net/ProcessImage"
+    url = "https://us-central1-theinternetofthings.cloudfunctions.net/DefineCard"
     query_string = "?"
     query_string += "image="+img_url
     r = requests.get(url + query_string)
@@ -20,10 +20,11 @@ def getImageText(img_url):
 
 if __name__ == '__main__':
     print("Testing mode....")
-    img_url = "https://media.discordapp.net/attachments/564894502725222400/630175584613433364/JPEG_20191005_185242.jpg"
+    img_url = "https://media.discordapp.net/attachments/564894502725222400/630160859183710219/20191005_175336.jpg"
     #img_url = "http://edge.rit.edu/edge/P15482/public/Photo Gallery/RIT_logo.jpg"
     print("Getting image: " + img_url + "...")
-    resp = getImageContents(img_url)
+    resp = getImageText(img_url)
     print("Response received!")
-    for entry in resp:
-        print(entry)
+    print(resp)
+    # for entry in resp:
+    #    print(entry)
--- theinternetofthings/utils/mongoUtils.py
+++ theinternetofthings/utils/mongoUtils.py
@@ -7,21 +7,31 @@ users = db.users
 stopwords = db.stopwords
 
 def createThing(thing):
-    things.insert_one({
-        "likes": 0,
-        "dislikes": 0,
-        "name": thing["name"],
-        "mid": thing["mid"],
-    })
+    if not getThing(thing["mid"]):
+        things.insert_one({
+            "likes": 0,
+            "dislikes": 0,
+            "name": thing["name"],
+            "mid": thing["mid"],
+        })
 
 def getThing(mid):
-    db.things.find({
+    found = things.find_one({
         "mid": mid
     })
+    return found
 
+def like(mid):
+    things.update({"mid": mid}, {"$inc": {"likes": 1}})
+
+def dislike(mid):
+    things.update({"mid": mid}, {"$inc": {"dislikes": 1}})
 
 if __name__ == '__main__':
     import cloudFunctions
     resp = cloudFunctions.getImageContents("http://edge.rit.edu/edge/P15482/public/Photo Gallery/RIT_logo.jpg")
     createThing(resp[0])
+    print(getThing(resp[0]["mid"]))
+    like(resp[0]["mid"])
+    dislike(resp[0]["mid"])
     print(getThing(resp[0]["mid"]))
